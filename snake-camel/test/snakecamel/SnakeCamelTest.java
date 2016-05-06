ec8b3f7b8087662ff6d5e6cf1c1303c7dd23d1b5
g14944sy <g14944sy@s30459.tsuda.ac.jp>
2016-05-06 11:15:54 +0900

    test

---
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -0,0 +1,14 @@
+package snakecamel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeCamelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
